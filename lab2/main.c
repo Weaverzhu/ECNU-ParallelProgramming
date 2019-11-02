de1d2d1c19fca82c9aa9778083a87b0ed717baf5
Weaverzhu <weaver_zhu@outlook.com>
2019-11-02 10:47:06 +0800

    start to work in local

--- lab2/main.c
+++ lab2/main.c
@@ -3,9 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 int main(int argc, char const *argv[])
 {
-    
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int number;
 
     
     return 0;
