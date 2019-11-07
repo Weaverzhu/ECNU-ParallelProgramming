f7d4f2584de1e6c60422e81d9e50701fe05ac257
Weaverzhu <weaver_zhu@outlook.com>
2019-11-07 10:08:51 +0800

    fix requirement

--- lab3/parallel.c
+++ lab3/parallel.c
@@ -21,37 +21,19 @@ int main(int argc, char **argv)
 
     // -------------------------------------
 
-    double sx, sy,  cx = 1, cy = 1;
-    int seed = atoi(argv[1]); srand(seed);
-    int num_try = atoi(argv[2]) / 4;
-    switch (world_rank)
-    {
-    case 0:
-        sx = sy = 0;
-        break;
-    case 1:
-        sx = sy = 1;
-        break;
-    case 2:
-        sx = 0; sy = 1;
-        break;
-    case 3:
-        sx = 1; sy = 0;
-        break;
-    
-    default:
-        break;
+    int num_try = atoi(argv[1]), hit = 0;
+    int cx = 1, cy = 1;
+    for (int i=0; i<num_try; ++i) {
+        double x = randd(2), y = randd(2), dx = x - cx, dy = y - cy;
+        if (dx * dx + dy * dy < 1) {
+            ++hit;
+        }
     }
 
-    int hit = 0, global_hit = 0;
-    for (int i=0; i<num_try; ++i) {
-        double x = sx + randd(1), y = sy + randd(1), dx = x - cx, dy = y - cy;
-        if (dx*dx + dy*dy <= 1) ++hit;
-    }
+    int global_hit = 0, global_try = world_size * num_try;
     MPI_Reduce(&hit, &global_hit, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (world_rank == 0) {
-        printf("In proc %d, Pi = %.4lf\n", world_rank, 4.0 * global_hit / (num_try * world_size));
-    }
+    if (world_rank == 0) 
+        printf("In total: %.2lf\n", 4.0 * global_hit / global_try);
 
     // -------------------------------------
     MPI_Finalize();
